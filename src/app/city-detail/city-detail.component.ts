01766fa0716d1da57f552a42a9846d9070e677af
Crystal C <crystal.contr@gmail.com>
2018-11-30 12:01:32 -0600

    Removed Input from city-details.components.ts and added CSS to stylesheet.

--- src/app/city-detail/city-detail.component.ts
+++ src/app/city-detail/city-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -11,7 +11,7 @@ import { CityService } from '../city.service';
   styleUrls: ['./city-detail.component.css']
 })
 export class CityDetailComponent implements OnInit {
-  @Input() city: City;
+  city: City;
 
   constructor(
     private route: ActivatedRoute,
