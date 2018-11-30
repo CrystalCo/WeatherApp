d709d08117add6f4367fb77956f733b563f1658c
Crystal C <crystal.contr@gmail.com>
2018-11-30 09:21:01 -0600

    Created a city service

--- src/app/city-detail/city-detail.component.ts
+++ src/app/city-detail/city-detail.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { City } from '../city';
 
-@Input() city: City;
-
 @Component({
   selector: 'app-city-detail',
   templateUrl: './city-detail.component.html',
   styleUrls: ['./city-detail.component.css']
 })
 export class CityDetailComponent implements OnInit {
+  @Input() city: City;
 
   constructor() { }
 
